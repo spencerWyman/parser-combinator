2a88e6f1ac99171df5ba108b45e58004660b7b4e
Spencer Wyman <spencer.wyaman7@gmail.com>
2020-03-01 05:49:27 -0800

    built test file to run

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+IDEA
+want to build a compiler, something where I can run
+`my-compiler code-file` it will run the code in the code file
+
+FEATURES
+What should this simple language have?
+-
+*/
