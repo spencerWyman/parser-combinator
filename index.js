641d8f9b45fceae821116a978f4eb06d3cd78323
Spencer Wyman <spencer.wyaman7@gmail.com>
2020-03-01 06:00:20 -0800

    stores content of input file

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 /*
 IDEA
 want to build a compiler, something where I can run
@@ -5,5 +8,12 @@ want to build a compiler, something where I can run
 
 FEATURES
 What should this simple language have?
--
+- data types: integers, strings, objects
+- variables: data vars, function vars
+- built in operators: + - * / ^ ( ) for arithmetic and strings
 */
+
+console.log(process.argv);
+code = fs.readFileSync(path.join(__dirname, process.argv[2]));
+
+console.log('code ' + code);
